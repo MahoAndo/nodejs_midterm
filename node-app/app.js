3676bf814a8793f65469c1338f9fe8d6dd5262f5
Maho Ando <111376661+MahoAndo@users.noreply.github.com>
2023-06-03 01:44:06 +0900

    Update app.js

--- node-app/app.js
+++ node-app/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
